94a9d47b5a40475d6e3fe4c7498071d1a2ca3a02
Mohammad Abbas <mohammad.abbas@digital.trade.gov.uk>
2022-08-16 16:04:16 +0100

    feature: pull key from s3 and push back to s3 in run-loader

--- fds_loader/run-loader.sh
+++ fds_loader/run-loader.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-
 # TODO: If we can't get aut working we may need to copy the key, data db and xml config from s3
 
 # Set the data loader credentials
 sed -i -e "s/<serial><\/serial>/<serial>$FACTSET_DEVICE_SERIAL<\/serial>/g" /fds_loader/config.xml
 sed -i -e "s/<user><\/user>/<user>$FACTSET_USER<\/user>/g" /fds_loader/config.xml
 
+# Pull key from s3
+aws s3 cp s3://$S3_OUTPUT_BUCKET/key.txt /fds_loader
+
 # Run the loader script
 /fds_loader/FDSLoader64 --bundle $FACTSET_BUNDLES
+
+# Copy key back to s3
+aws s3 cp /fds_loader/key.txt s3://$S3_OUTPUT_BUCKET
 
 # Copy downloaded files to s3 (uses minio if local endpoint is set)
 if [[ -z "${S3_LOCAL_ENDPOINT_URL}" ]]; then
